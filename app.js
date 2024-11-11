86d01c9f61b998a2385254f59f551c781640c41f
goswamisagar <148305515+goswamisagar@users.noreply.github.com>
2024-11-11 11:57:38 +0530

    app.js

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ function levelUp() {
     level++;
     h2.innerText = `Level ${level}`;
 
-    let randIdx = Math.floor(Math.random() * 3);
+    let randIdx = Math.floor(Math.random() * 4);
     let randcolor = btns[randIdx];
     let randBtn = document.querySelector(`.${randcolor}`);
     gameSeq.push(randcolor);
